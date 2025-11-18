e5fdc03cd42a329e6bd58585afc0575466e265aa
mijeong09 <me9809180@gmail.com>
2025-11-18 11:26:15 +0900

    read 로직 작성

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -5,13 +5,21 @@ import com.example.todoapp.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class TodoController {
+    private final TodoRepository todoRepository = new TodoRepository();
 
     @GetMapping("/todos")
-    public String todos() {
+    public String todos(Model model) {
+        // 이전에 만들었던 repository와 다른 객체를 사용하는거라 X
+        // TodoRepository todoRepository = new TodoRepository();
+        List<TodoDto> todos = todoRepository.findAll();
+        model.addAttribute("todos", todos);
         return "todos";
     }
 
@@ -27,11 +35,19 @@ public class TodoController {
             Model model
     ) {
         TodoDto todoDto = new TodoDto(null, title, content, false);
-        TodoRepository todoRepository = new TodoRepository();
+        // TodoRepository todoRepository = new TodoRepository();
 
         TodoDto todo = todoRepository.save(todoDto);
         model.addAttribute("todo", todo);
 
-        return "create";
+//        return "create";
+        return "redirect:/todos";
+    }
+
+    @GetMapping("/todos/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        TodoDto todo = todoRepository.findById(id);
+        model.addAttribute("todo", todo);
+        return "detail";
     }
 }
